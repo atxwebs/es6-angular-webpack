90a1e90b49a16c3b71b04363f1cebf55032de7b1
Seth Bergman <seth.atxwebs@gmail.com>
2016-12-25 01:31:01 -0600

    Added server.js to index.js

--- app/index.js
+++ app/index.js
@@ -5,3 +5,13 @@ const CommonModule = angular.module('common', [])
   .service('CategoriesModel', CategoriesModel);
 
 export default CommonModule;
+
+
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(process.env.PORT || 5000, function () {
+  console.log('Server running at http://0.0.0.0:' + server.address().port)
+})
