ce493202638138344e4e3a6aae6bd43f0edbbadc
Seth Bergman <seth.atxwebs@gmail.com>
2016-12-25 01:25:48 -0600

    Added data and generator structure and fixed gulp to listen on all addresses

---
+++ gulpfile.babel.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import gulp from 'gulp';
+import path from 'path';
+import webpack from 'webpack-stream';
+const browserSync = require('browser-sync').create();
+
+const reload = () => browserSync.reload();
+const root = 'public';
+
+// helper method for resolving paths
+const resolveToApp = (glob) => {
+  glob = glob || '';
+  return path.join(root, 'app', glob); // app/{glob}
+};
+
+// map of all paths
+const paths = {
+  js: resolveToApp('**/*!(.spec.js).js'), // exclude spec files
+  styl: resolveToApp('**/*.styl'), // stylesheets
+  html: [
+    resolveToApp('**/*.html'),
+    path.join(root, 'index.html')
+  ],
+  entry: path.join(root, 'app/app.js'),
+  //output: root
+};
+
+gulp.task('webpack', () => {
+  return gulp.src(paths.entry)
+    .pipe(webpack(require('./webpack.config')))
+    .pipe(gulp.dest(paths.output));
+});
+
+gulp.task('reload', ['webpack'], (done) => {
+  reload();
+  done();
+});
+
+
+gulp.task('serve', () => {
+  browserSync.init({
+    port: process.env.PORT || 5000,
+    server: { baseDir: root, domain: '0.0.0.0' }
+  });
+});
+
+gulp.task('watch', ['serve'], () => {
+  const allPaths = [].concat([paths.js], paths.html, [paths.styl]);
+  gulp.watch(allPaths, ['reload']);
+});
+
+gulp.task('default', ['watch']);
